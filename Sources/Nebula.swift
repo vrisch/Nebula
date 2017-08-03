65c2bacedef67de002adbe3f27a724a987bb504b
Magnus Nilsson <magnus.nilsson@smartrefill.se>
2017-08-03 15:09:16 +0200

    More movement

--- Sources/Nebula.swift
+++ Sources/Nebula.swift
@@ -33,24 +33,31 @@ public enum Change<T> {
         public var changed: T
         public var added: T
         public var removed: T
+        public var moved: T
     }
-    
+
     /*
      * If mode = .all every value is added to the "changed" list regardless of changes status
      * If mode = .element then inserted and updated are added to the "changed" list, no values are added to "added" and "removed"
      * If mode = .list then inserted are added to the "added" list and deleted to the "removed" list, no values are added to "changed"
      */
     public static func delta<S: Sequence>(_ changes: S, _ mode: Mode) -> Delta<[T]> where S.Element == Change<T> {
-        var result: Delta<[T]> = Delta<[T]>(changed: [], added: [], removed: [])
+        var result: Delta<[T]> = Delta<[T]>(changed: [], added: [], removed: [], moved: [])
         var hasMovement = false
         changes.forEach { change in
             switch (mode, change) {
             case (.all, _):
                 result.changed.append(change.value)
-            case (.element, .inserted), (.element, .updated):
-                result.changed.append(change.value)
+
+            case (.element, .deleted):
+                result.removed.append(change.value)
+            case (.element, .inserted):
+                result.added.append(change.value)
             case (.element, .unchanged):
-                if hasMovement { result.changed.append(change.value) }
+                if hasMovement { result.moved.append(change.value) }
+            case (.element, .updated):
+                result.changed.append(change.value)
+
             case (.list, .deleted):
                 result.removed.append(change.value)
             case (.list, .inserted):
@@ -86,20 +93,26 @@ public enum Change<T> {
     }
 
     public static func count<S: Sequence>(_ changes: S) -> Delta<Int> where S.Element == Change<T> {
-        var result: Delta<Int> = Delta<Int>(changed: 0, added: 0, removed: 0)
+        var result: Delta<Int> = Delta<Int>(changed: 0, added: 0, removed: 0, moved: 0)
+        var hasMovement = false
         for change in changes {
             switch change {
             case .deleted: result.removed += 1
             case .inserted: result.added += 1
-            case .unchanged: break
+            case .unchanged: if hasMovement { result.moved += 1 }
             case .updated: result.changed += 1
+            }
+            switch change {
+            case .inserted, .deleted: hasMovement = true
+            default: break
             }
         }
         return result
     }
 
     public static func hasChanges<S: Sequence>(_ changes: S) -> Bool where S.Element == Change<T> {
-        return !count(changes).isEmpty
+        let delta = count(changes)
+        return delta.changed > 0 || delta.added > 0 || delta.removed > 0
     }
 
     public static func normalized<S: Sequence>(_ changes: S) -> [Change] where S.Element == Change<T> {
@@ -113,13 +126,6 @@ public enum Change<T> {
             }
         }
         return result
-    }
-}
-
-extension Change.Delta where T == Int {
-    
-    public var isEmpty: Bool {
-        return changed == 0 && added == 0 && removed == 0
     }
 }
 
