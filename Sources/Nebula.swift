72dd7ff83a60afd5dcbc2aa8585d9f48c7e60307
Vrisch <magnus@vrischikasana.se>
2017-08-27 16:26:51 +0200

    Added Views

--- Sources/Nebula.swift
+++ Sources/Nebula.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-public protocol Model {}
+public protocol Model: Equatable {}
 
 public enum Change<T: Model> {
     case deleted(T)
@@ -45,6 +45,108 @@ public struct Delta<T> {
         self.added = added
         self.removed = removed
         self.moved = moved
+    }
+}
+
+public struct View<T: Model> {
+    public var indexes: Delta<[Int]> = Delta(mode: .initial, changed: [], added: [], removed: [], moved: [])
+
+    public init(by areInIncreasingOrder: @escaping (T, T) -> Bool) {
+        self.areInIncreasingOrder = areInIncreasingOrder
+        self.orderedView = []
+    }
+    
+    public mutating func process(delta: Delta<[T]>) {
+        indexes = Delta(mode: delta.mode, changed: [], added: [], removed: [], moved: [])
+        
+        guard delta.mode != .initial else {
+            orderedView = delta.changed.map { .unchanged($0) }
+            orderedView = orderedView.sorted(by: { (lhs, rhs) in
+                return areInIncreasingOrder(lhs.value, rhs.value)
+            })
+            return
+        }
+        
+        // Deletes must be processed first, since the indexes are relative to the old content
+        delta.removed.forEach { element in
+            if let index = orderedView.index(where: { $0.value == element }) {
+                indexes.removed.append(index)
+            }
+        }
+        
+        // Now that the removed indexes are recorded, we can go ahead and delete the elements (in reverse order)
+        indexes.removed.reversed().forEach { orderedView.remove(at: $0) }
+        
+        // Now process inserts and changes without recording indexes
+        delta.added.forEach { element in
+            orderedView.append(.inserted(element))
+        }
+        delta.changed.forEach { element in
+            if let index = orderedView.index(where: { $0.value == element }) {
+                orderedView[index] = .updated(element)
+            }
+        }
+        
+        // Sort the new updated content
+        orderedView = orderedView.sorted(by: { (lhs, rhs) in
+            return areInIncreasingOrder(lhs.value, rhs.value)
+        })
+        
+        // Find the inserted and changed indexes
+        delta.added.forEach { element in
+            if let index = orderedView.index(where: { $0.value == element }) {
+                indexes.added.append(index)
+            }
+        }
+        delta.changed.forEach { element in
+            if let index = orderedView.index(where: { $0.value == element }) {
+                indexes.changed.append(index)
+            }
+        }
+    }
+
+    private var orderedView: [Change<T>]
+    private let areInIncreasingOrder: (T, T) -> Bool
+}
+
+extension View: Sequence {
+    public typealias Iterator = ViewIterator<T>
+
+    public struct ViewIterator<T: Model>: IteratorProtocol {
+        public typealias Element = T
+        
+        init(_ iterator: IndexingIterator<[Change<T>]>) {
+            self.iterator = iterator
+        }
+        
+        public mutating func next() -> T? {
+            return iterator.next()?.value
+        }
+        private var iterator: IndexingIterator<[Change<T>]>
+    }
+
+    public func makeIterator() -> Iterator {
+        return ViewIterator(orderedView.makeIterator())
+    }
+}
+
+extension View: Collection {
+    public typealias Index = Int
+    
+    public var startIndex: Index {
+        return orderedView.startIndex
+    }
+    
+    public var endIndex: Index {
+        return orderedView.endIndex
+    }
+    
+    public subscript (position: Index) -> Iterator.Element {
+        return orderedView[position].value
+    }
+
+    public func index(after i: Index) -> Index {
+        return orderedView.index(after: i)
     }
 }
 
--- Tests/NebulaTests/NebulaTests.swift
+++ Tests/NebulaTests/NebulaTests.swift
@@ -10,8 +10,14 @@ import Foundation
 import XCTest
 import Nebula
 
-struct Test: Model {
+extension String: Model {}
+
+struct Test: Model, Equatable {
     let id: String
+
+    static func ==(lhs: Test, rhs: Test) -> Bool {
+        return lhs.id == rhs.id
+    }
 }
 
 class NebulaTests: XCTestCase {
@@ -19,11 +25,89 @@ class NebulaTests: XCTestCase {
     func testConvertable() {
         var state: [String: Test] = [:]
         let data: [Change<Test>] = []
-        let delta = data.delta(mode: .all)
+        let delta = data.delta(mode: .initial)
         delta.changed.forEach { state[$0.id] = $0 }
         XCTAssertEqual(delta.isEmpty, true)
     }
     
+    func testView1() {
+        var view = View<String>(by: <)
+        let delta = Delta<[String]>(mode: .initial, changed: ["Banana", "Apple", "Strawberry"], added: [], removed: [], moved: [])
+
+        view.process(delta: delta)
+
+        XCTAssertEqual(view.isEmpty, false)
+        XCTAssertEqual(view[0], "Apple")
+        XCTAssertEqual(view[1], "Banana")
+        XCTAssertEqual(view[2], "Strawberry")
+        XCTAssertEqual(view.indexes.mode, .initial)
+        XCTAssertEqual(view.indexes.changed, [])
+        XCTAssertEqual(view.indexes.added, [])
+        XCTAssertEqual(view.indexes.removed, [])
+        XCTAssertEqual(view.indexes.moved, [])
+    }
+    
+    func testView2() {
+        var view = View<String>(by: <)
+        let delta1 = Delta<[String]>(mode: .initial, changed: ["Banana", "Apple", "Strawberry"], added: [], removed: [], moved: [])
+        
+        view.process(delta: delta1)
+        
+        let delta2 = Delta<[String]>(mode: .list, changed: [], added: ["Cherry"], removed: [], moved: [])
+        
+        view.process(delta: delta2)
+        
+        XCTAssertEqual(view.isEmpty, false)
+        XCTAssertEqual(Array(view), ["Apple", "Banana", "Cherry", "Strawberry"])
+        XCTAssertEqual(view.indexes.mode, .list)
+        XCTAssertEqual(view.indexes.changed, [])
+        XCTAssertEqual(view.indexes.added, [2])
+        XCTAssertEqual(view.indexes.removed, [])
+        XCTAssertEqual(view.indexes.moved, [])
+    }
+    
+    func testView3() {
+        var view = View<String>(by: <)
+        let delta1 = Delta<[String]>(mode: .initial, changed: ["Banana", "Apple", "Strawberry"], added: [], removed: [], moved: [])
+        
+        view.process(delta: delta1)
+        
+        let delta2 = Delta<[String]>(mode: .list, changed: ["Apple"], added: ["Cherry"], removed: ["Strawberry"], moved: [])
+
+        view.process(delta: delta2)
+        
+        XCTAssertEqual(view.isEmpty, false)
+        XCTAssertEqual(Array(view), ["Apple", "Banana", "Cherry"])
+        XCTAssertEqual(view.indexes.mode, .list)
+        XCTAssertEqual(view.indexes.changed, [0])
+        XCTAssertEqual(view.indexes.added, [2])
+        XCTAssertEqual(view.indexes.removed, [2])
+        XCTAssertEqual(view.indexes.moved, [])
+    }
+    
+    func testView4() {
+        var view = View<String>(by: <)
+        let delta1 = Delta<[String]>(mode: .initial, changed: ["Banana", "Apple", "Strawberry"], added: [], removed: [], moved: [])
+        
+        view.process(delta: delta1)
+        
+        let delta2 = Delta<[String]>(mode: .list, changed: ["Apple"], added: ["Cherry"], removed: ["Strawberry"], moved: [])
+        
+        view.process(delta: delta2)
+        
+        let delta3 = Delta<[String]>(mode: .list, changed: ["Cherry", "Banana"], added: ["Pineapple"], removed: [], moved: [])
+        
+        view.process(delta: delta3)
+
+        XCTAssertEqual(view.isEmpty, false)
+        XCTAssertEqual(Array(view), ["Apple", "Banana", "Cherry", "Pineapple"])
+        XCTAssertEqual(view.indexes.mode, .list)
+        XCTAssertEqual(view.indexes.changed, [2, 1])
+        XCTAssertEqual(view.indexes.added, [3])
+        XCTAssertEqual(view.indexes.removed, [])
+        XCTAssertEqual(view.indexes.moved, [])
+    }
+
     static var allTests = [
         ("testConvertable", testConvertable),
     ]
