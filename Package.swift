9a060ee59bbfbc424d779e035237f39b171d80f4
Magnus Nilsson <magnus.nilsson@smartrefill.se>
2017-11-03 10:23:24 +0100

    SPM

--- Package.swift
+++ Package.swift
@@ -1,5 +1,19 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-    name: "Nebula"
+    name: "Nebula",
+    products: [
+        .library(
+            name: "Nebula",
+            targets: ["Nebula"]),
+        ],
+    dependencies: [
+        .package(url: "https://github.com/vrisch/Orbit.git", .branch("develop")),
+        ],
+    targets: [
+        .target(
+            name: "Nebula",
+            dependencies: ["Orbit"]),
+        ]
 )
